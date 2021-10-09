4760672dbafdaae766b97a4a7cb471a9d14442aa
Daan Asscheman <dasscheman@gmail.com>
2021-10-09 17:40:43 +0200

    bugje

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -86,7 +86,7 @@ class SiteController extends Controller
             ],
             'captcha' => [
                 'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => $_ENV['YII_ENV'] == 'dev ? 'testme' : null,
+                'fixedVerifyCode' => $_ENV['YII_ENV'] == 'dev' ? 'testme' : null,
             ],
         ];
     }
